593e8cc1cf79e190a3f5c1a2517e8be4435e6d2c
mira-kine <mirak0419@gmail.com>
2022-04-01 11:06:29 -0700

    wrote test for get posts first, controller passing

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const GithubUser = require('../lib/models/GithubUser');
 
 jest.mock('../lib/utils/github');
 
@@ -15,22 +14,46 @@ describe('gitty routes', () => {
     pool.end();
   });
 
-  it('user can create short 255 char post if signed in', async () => {
+  it('user can get posts after redirect from oauth', async () => {
     const agent = request.agent(app);
-    // sign in gitHub user
-    await GithubUser.insert({
-      username: 'fake_github_user',
-      avatar: 'https://www.placecage.com/gif/300/300',
-      email: 'not-real@example.com',
-    });
-    await agent.get('/login/callback');
-
-    const res = await agent.post('/api/v1/posts/create').send({
-      text: 'this is my test post',
-    });
-    expect(res.body).toEqual({
+    await agent.get('/api/v1/github/login');
+    // call back redirect, exchange access_token
+    await request
+      .agent(app)
+      .get('/api/v1/github/login/callback?code=42')
+      .redirects(1);
+    // posts
+    const post1 = {
       id: expect.any(String),
       text: 'this is my test post',
-    });
+    };
+
+    const post2 = {
+      id: expect.any(String),
+      text: 'this is my second test post',
+    };
+
+    const res = await agent.get('/api/v1/posts');
+
+    expect(res.body).toEqual([post1, post2]);
   });
+
+  // it('user can create short 255 char post if signed in', async () => {
+  //   const agent = request.agent(app);
+  //   const loginReq = await request
+  //     .agent(app)
+  //     .get('/api/v1/github/login/callback?code=42')
+  //     .redirects(1);
+  //   // sign in gitHub user
+  //   // mocks give fake token
+  //   const callbackRes = await agent.get('/login/callback');
+
+  //   const res = await agent.post('/api/v1/posts/create').send({
+  //     text: 'this is my test post',
+  //   });
+  //   expect(res.body).toEqual({
+  //     id: expect.any(String),
+  //     text: 'this is my test post',
+  //   });
+  // });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -12,5 +12,5 @@ CREATE TABLE github_users (
 
 CREATE TABLE posts (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    text VARCHAR(255) not null
+    text VARCHAR(255) NOT NULL
 );
